9e40b42afcf22acb5450c7b7276ee36c4b36dd7c
xuzhiyi1998 <1580987871@qq.com>
2020-01-21 21:18:54 +0800

    增加了设置文件

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,17 @@
 import sys
 import pygame
+from settings import Settings
+settings=Settings()
 def run_game():
     pygame.init()
-    screen=pygame.display.set_mode((1200,800))
+    screen=pygame.display.set_mode(settings.screen)
     pygame.display.set_caption("外星人入侵")#设置窗口标题
     bg_color=(230,230,230)
     while True:#开始游戏循环
         for event in pygame.event.get():#pygame.event.get()返回值是一个列表
             if event.type==pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(settings.color)
         pygame.display.flip()#让最近绘制的屏幕可见
 run_game()
+print(sys.path)
---
+++ settings.py
@@ -0,0 +1,4 @@
+class Settings():
+    def __init__(self):
+        self.screen=(1200,800)
+        self.color=(230,230,230)
